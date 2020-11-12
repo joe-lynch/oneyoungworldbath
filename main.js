fbb0b330f19d70fe9ed70e2d0c758958968efbb6
joe-lynch <jl2553@bath.ac.uk>
2020-11-12 14:53:50 +0000

    changed layout

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
  
  $(".btn").mousedown(function(e){
-	e.preventDefault();
+    e.preventDefault();
 });
 
 $('.btn').mouseup(function(){
-	$(this).css("color", "white");
+    $(this).css("color", "white");
 });
 
 $('.btn').mouseover(function(){
-	$(this).css("color", "black");
+    $(this).css("color", "black");
 });
 
 $('.btn').mouseout(function(){
-	$(this).css("color", "white");
+    $(this).css("color", "white");
 });
 
 $("#info_btn").click(function(){
   $("html,body").animate({
-	  scrollTop: $('#page_pg').offset().top - $('body').offset().top + $('body').scrollTop()
+      scrollTop: $('#page_pg').offset().top - $('body').offset().top + $('body').scrollTop()
   },'slow');
 });
 
 $("#contact_btn").click(function(){
-	$("html,body").animate({
-	  scrollTop: $('#watch-this-space').offset().top - $('body').offset().top + $('body').scrollTop()
+    $("html,body").animate({
+      scrollTop: $('#watch-this-space').offset().top - $('body').offset().top + $('body').scrollTop()
   },1100);
 });
 
@@ -33,31 +33,31 @@ $("#contact_btn").click(function(){
 /*
 function CountDownTimer(dt, id)
 {
-	var end = new Date(dt);
-	var _second = 1000;
-	var _minute = _second * 60;
-	var _hour = _minute * 60;
-	var _day = _hour * 24;
-	var _week = _day * 7;
-	var timer;
-	
-	function showRemaining() {
-		
-		var now = new Date();
-		var distance = end - now;
-		var weeks = Math.floor(distance / _week);
-		var days = Math.floor((distance % _week) / _day);
-		var hours = Math.floor((distance % _day) / _hour);
-		var minutes = Math.floor((distance % _hour) / _minute);
-		var seconds = Math.floor((distance % _minute) / _second);
-		
-		document.getElementById("weeks").innerHTML = weeks;
-		document.getElementById("days").innerHTML = days;
-		document.getElementById("hours").innerHTML = hours;
-		document.getElementById("minutes").innerHTML = minutes
-		document.getElementById("seconds").innerHTML = seconds;
-	}
-	
-	timer = setInterval(showRemaining, 1000);
+    var end = new Date(dt);
+    var _second = 1000;
+    var _minute = _second * 60;
+    var _hour = _minute * 60;
+    var _day = _hour * 24;
+    var _week = _day * 7;
+    var timer;
+    
+    function showRemaining() {
+        
+        var now = new Date();
+        var distance = end - now;
+        var weeks = Math.floor(distance / _week);
+        var days = Math.floor((distance % _week) / _day);
+        var hours = Math.floor((distance % _day) / _hour);
+        var minutes = Math.floor((distance % _hour) / _minute);
+        var seconds = Math.floor((distance % _minute) / _second);
+        
+        document.getElementById("weeks").innerHTML = weeks;
+        document.getElementById("days").innerHTML = days;
+        document.getElementById("hours").innerHTML = hours;
+        document.getElementById("minutes").innerHTML = minutes
+        document.getElementById("seconds").innerHTML = seconds;
+    }
+    
+    timer = setInterval(showRemaining, 1000);
 }
 */
